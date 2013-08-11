a36fe3e46a14ef75fe93e82bbf806ec74d807392
Stephen Nelson <stephenenelson@mac.com>
2013-08-10 23:41:07 -0700

    Fixed date format

--- public/js/filters.js
+++ public/js/filters.js
@@ -4,7 +4,7 @@ angular.module('pmuiFilters', []).
 	filter('dateformat', function() {
 		return function(input) {
 			var now = moment.unix(input);
-			var formatted = now.format('ddd HH:MM:SS');
+			var formatted = now.format('ddd HH:mm:ss');
 			return formatted;
 	    }
 	});	
